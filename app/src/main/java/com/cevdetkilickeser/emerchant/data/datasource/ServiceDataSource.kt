ff9750eb687832c74e1975206d35d3380e3a68b8
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-06-19 14:50:58 +0300

    ServiceDataSource rearranged

--- app/src/main/java/com/cevdetkilickeser/emerchant/data/datasource/ServiceDataSource.kt
+++ app/src/main/java/com/cevdetkilickeser/emerchant/data/datasource/ServiceDataSource.kt
@@ -1,9 +1,14 @@
 package com.cevdetkilickeser.emerchant.data.datasource
 
+import com.cevdetkilickeser.emerchant.data.entity.product.Product
 import com.cevdetkilickeser.emerchant.retrofit.ServiceDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class ServiceDataSource(val sdao: ServiceDao) {
+class ServiceDataSource(private val serviceDao: ServiceDao) {
 
-    suspend fun login(email: String, password: String) = sdao.login(email, password)
-
+    suspend fun getProducts() : List<Product> =
+        withContext(Dispatchers.IO) {
+            return@withContext serviceDao.getProducts().products
+        }
 }
