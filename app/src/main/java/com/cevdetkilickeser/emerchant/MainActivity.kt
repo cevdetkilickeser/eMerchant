d1ff25499603cba99aae55965a6c2473959ebab8
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-06-20 13:53:44 +0300

    toggle and onbackpressed function added

--- app/src/main/java/com/cevdetkilickeser/emerchant/MainActivity.kt
+++ app/src/main/java/com/cevdetkilickeser/emerchant/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.cevdetkilickeser.emerchant
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.cevdetkilickeser.emerchant.databinding.ActivityMainBinding
+import com.cevdetkilickeser.emerchant.databinding.DrawerHeaderBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var headerBinding: DrawerHeaderBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,5 +24,30 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         NavigationUI.setupWithNavController(binding.navigationView, navHostFragment.navController)
+
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            0,
+            0
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val headerView = binding.navigationView.getHeaderView(0)
+        headerBinding = DrawerHeaderBinding.bind(headerView)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
 }
