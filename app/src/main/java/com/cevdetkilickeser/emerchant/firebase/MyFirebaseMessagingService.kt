214a6fe9e74ba76688303864832083cd63599955
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-06-26 16:37:10 +0300

    Push Notification added

---
+++ app/src/main/java/com/cevdetkilickeser/emerchant/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,77 @@
+package com.cevdetkilickeser.emerchant.firebase
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import com.cevdetkilickeser.emerchant.R
+import com.cevdetkilickeser.emerchant.ui.activity.MainActivity
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val channelId = "notification_channel"
+const val channelName = "com.cevdetkilickeser.emerchant"
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.notification != null) {
+            generateNotification(
+                remoteMessage.notification!!.title!!,
+                remoteMessage.notification!!.body!!
+            )
+        }
+    }
+
+    private fun generateNotification(title: String, message: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent =
+            PendingIntent.getActivity(
+                this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+        var builder: NotificationCompat.Builder =
+            NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.mipmap.app_icon)
+                .setAutoCancel(true)
+                .setVibrate(longArrayOf(100, 100, 100, 100))
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(title, message))
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notificationManager.notify(0, builder.build())
+    }
+
+    private fun getRemoteView(title: String, message: String): RemoteViews {
+        val remoteView = RemoteViews("com.cevdetkilickeser.emerchant", R.layout.notification)
+
+        remoteView.setTextViewText(R.id.title, title)
+        remoteView.setTextViewText(R.id.message, message)
+        remoteView.setImageViewResource(R.id.app_logo, R.mipmap.app_icon)
+
+        return remoteView
+    }
+
+
+}
