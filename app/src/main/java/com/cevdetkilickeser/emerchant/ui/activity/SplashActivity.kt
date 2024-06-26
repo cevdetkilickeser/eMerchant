8663c79f2583f7ba2058837b8fad93978dc31bca
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-06-26 18:19:23 +0300

    Splash Screen rearranged

--- app/src/main/java/com/cevdetkilickeser/emerchant/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/cevdetkilickeser/emerchant/ui/activity/SplashActivity.kt
@@ -5,19 +5,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.cevdetkilickeser.emerchant.databinding.ActivitySplashScreenBinding
+import com.cevdetkilickeser.emerchant.databinding.ActivitySplashBinding
 import com.cevdetkilickeser.emerchant.ui.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var binding: ActivitySplashBinding
     private lateinit var viewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.imageViewIcon.alpha = 0f
 
         val tempViewModel: LoginViewModel by viewModels()
         viewModel = tempViewModel
@@ -37,15 +39,19 @@ class SplashActivity : AppCompatActivity() {
                 editor.putString("userId", user.id.toString())
                 editor.apply()
 
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                intent.putExtra("user", it)
-                startActivity(intent)
-                finish()
+                binding.imageViewIcon.animate().setDuration(1500).alpha(1f).withEndAction {
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    intent.putExtra("user", it)
+                    startActivity(intent)
+                    finish()
+                }
 
             } ?: run {
-                val intent = Intent(this@SplashActivity, LoginActivity::class.java)
-                startActivity(intent)
-                finish()
+                binding.imageViewIcon.animate().setDuration(1500).alpha(1f).withEndAction {
+                    val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
             }
         }
     }
