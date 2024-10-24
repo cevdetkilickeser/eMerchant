e0e799fae385209126fc53fca5e80a5f9a542052
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-24 15:11:36 +0300

    CartFragment -> two of onClickButton functions convert to one updateCart function. Code simplification was done

--- app/src/main/java/com/cevdetkilickeser/emerchant/ui/fragment/CartFragment.kt
+++ app/src/main/java/com/cevdetkilickeser/emerchant/ui/fragment/CartFragment.kt
@@ -8,11 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.cevdetkilickeser.emerchant.data.entity.cart.CartProduct
 import com.cevdetkilickeser.emerchant.databinding.FragmentCartBinding
 import com.cevdetkilickeser.emerchant.ui.adapter.CartProductAdapter
 import com.cevdetkilickeser.emerchant.ui.viewmodel.CartViewModel
+import com.cevdetkilickeser.emerchant.utils.formatPrice
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.Locale
 
 @AndroidEntryPoint
 class CartFragment : Fragment() {
@@ -30,29 +31,45 @@ class CartFragment : Fragment() {
         binding = FragmentCartBinding.inflate(layoutInflater, container, false)
 
         viewModel.cartProductList.observe(viewLifecycleOwner) { cartProductList ->
-            binding.cartProductsTotal.text = buildString {
-                append("$ ")
-                append(String.format(Locale.US, "%.2f", viewModel.cartTotal))
-            }
-
-            val cartProductAdapter =
-                CartProductAdapter(
-                    cartProductList,
-                    ::onClickButtonIncrease,
-                    ::onClickButtonDecrease
-                )
+            val cartProductAdapter = CartProductAdapter(cartProductList, ::updateCart)
             binding.rvCart.adapter = cartProductAdapter
         }
 
-        viewModel.isEmptyCart.observe(viewLifecycleOwner) { isEmptyCart ->
-            showEmptyCart(isEmptyCart)
+        viewModel.isProgress.observe(viewLifecycleOwner) { isProgress ->
+            showProgressBar(isProgress)
         }
 
-        viewModel.isProgress.observe(viewLifecycleOwner) { isProgress ->
-            showProgress(isProgress)
+        viewModel.isEmptyCart.observe(viewLifecycleOwner) { isEmpty ->
+            showEmptyCartImage(isEmpty)
+        }
+
+        viewModel.cart.observe(viewLifecycleOwner) { cart ->
+            binding.cartProductsTotal.text = formatPrice(cart?.total)
         }
 
         return binding.root
+    }
+
+    private fun showProgressBar(showProgress: Boolean) {
+        if (showProgress) {
+            binding.progressBarCart.visibility = View.VISIBLE
+        } else {
+            binding.progressBarCart.visibility = View.GONE
+        }
+    }
+
+    private fun showEmptyCartImage(isEmpty: Boolean) {
+        if (isEmpty) {
+            binding.imageViewEmptyCart.visibility = View.VISIBLE
+            binding.cardViewCartTotal.visibility = View.GONE
+        } else {
+            binding.imageViewEmptyCart.visibility = View.GONE
+            binding.cardViewCartTotal.visibility = View.VISIBLE
+        }
+    }
+
+    private fun updateCart(cartProduct: CartProduct, isUpdateUp: Boolean) {
+        viewModel.updateCart(userId, cartProduct, isUpdateUp)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,31 +82,5 @@ class CartFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         viewModel.getCart(userId)
-    }
-
-    private fun onClickButtonIncrease(cartProductId: Int, updateUp: Boolean) {
-        viewModel.updateCartProductQuantity(userId, cartProductId, updateUp)
-    }
-
-    private fun onClickButtonDecrease(cartProductId: Int, updateUp: Boolean) {
-        viewModel.updateCartProductQuantity(userId, cartProductId, updateUp)
-    }
-
-    private fun showEmptyCart(isEmptyCart: Boolean) {
-        if (isEmptyCart) {
-            binding.imageViewEmptyCart.visibility = View.VISIBLE
-            binding.cardViewCartTotal.visibility = View.GONE
-        } else {
-            binding.imageViewEmptyCart.visibility = View.GONE
-            binding.cardViewCartTotal.visibility = View.VISIBLE
-        }
-    }
-
-    private fun showProgress(isProgress: Boolean) {
-        if (isProgress) {
-            binding.progressBarCart.visibility = View.VISIBLE
-        } else {
-            binding.progressBarCart.visibility = View.GONE
-        }
     }
 }
