dc1e0995b0c6d1c3ca87edfb82cdfb3e07c68657
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-24 14:21:47 +0300

    formatPrice fun created to show 2 digit after comma on price text

--- app/src/main/java/com/cevdetkilickeser/emerchant/utils/Extension.kt
+++ app/src/main/java/com/cevdetkilickeser/emerchant/utils/Extension.kt
@@ -3,8 +3,16 @@ package com.cevdetkilickeser.emerchant.utils
 import android.content.Intent
 import android.os.Build.VERSION.SDK_INT
 import android.os.Parcelable
+import java.util.Locale
 
 inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
     SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
     else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+}
+
+fun formatPrice(price: Double?): String {
+    return buildString {
+        append("$ ")
+        append(String.format(Locale.US, "%.2f", price))
+    }
 }
