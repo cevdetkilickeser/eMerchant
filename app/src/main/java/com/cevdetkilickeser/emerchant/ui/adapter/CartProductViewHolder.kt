8a0739992fcd22b3db136368cd749dbdd159e4f2
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-24 15:13:09 +0300

    CartProductViewHolder -> two of onClickButton functions convert to one updateCart function. formatPrice fun  implemented

--- app/src/main/java/com/cevdetkilickeser/emerchant/ui/adapter/CartProductViewHolder.kt
+++ app/src/main/java/com/cevdetkilickeser/emerchant/ui/adapter/CartProductViewHolder.kt
@@ -4,32 +4,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.cevdetkilickeser.emerchant.data.entity.cart.CartProduct
 import com.cevdetkilickeser.emerchant.databinding.CartProductViewBinding
-import java.util.Locale
+import com.cevdetkilickeser.emerchant.utils.formatPrice
 
 class CartProductViewHolder(var binding: CartProductViewBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(
         cartProduct: CartProduct,
-        onClickButtonIncrease: (Int, Boolean) -> Unit,
-        onClickButtonDecrease: (Int, Boolean) -> Unit
+        updateCart: (CartProduct, Boolean) -> Unit
     ) {
         with(binding) {
             Glide.with(this.root).load(cartProduct.thumbnail).into(this.imageViewCartProductView)
             cartProductTitle.text = cartProduct.title
-            cartProductPrice.text = buildString {
-                append("$ ")
-                append(String.format(Locale.US, "%.2f", cartProduct.price))
-            }
+            cartProductPrice.text = formatPrice(cartProduct.price)
             cartProductQuantity.text = cartProduct.quantity.toString()
-            cartProductTotal.text = buildString {
-                append("$ ")
-                append(String.format(Locale.US, "%.2f", cartProduct.total))
-            }
+            cartProductTotal.text = formatPrice(cartProduct.total)
             buttonIncrease.setOnClickListener {
-                onClickButtonIncrease.invoke(cartProduct.id, true)
+                updateCart.invoke(cartProduct, true)
             }
+
             buttonDecrease.setOnClickListener {
-                onClickButtonDecrease.invoke(cartProduct.id, false)
+                updateCart.invoke(cartProduct, false)
             }
         }
     }
