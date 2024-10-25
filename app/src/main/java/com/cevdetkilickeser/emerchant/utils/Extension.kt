d18c36092116eb7ff1995617e2df5a2b3fd6f140
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-10-25 21:29:08 +0300

    Order, CartProduct -> mapCartToOrder function created

--- app/src/main/java/com/cevdetkilickeser/emerchant/utils/Extension.kt
+++ app/src/main/java/com/cevdetkilickeser/emerchant/utils/Extension.kt
@@ -3,6 +3,8 @@ package com.cevdetkilickeser.emerchant.utils
 import android.content.Intent
 import android.os.Build.VERSION.SDK_INT
 import android.os.Parcelable
+import com.cevdetkilickeser.emerchant.data.model.cart.Cart
+import com.cevdetkilickeser.emerchant.data.model.order.Order
 import java.util.Locale
 
 inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
@@ -15,4 +17,16 @@ fun formatPrice(price: Double?): String {
         append("$ ")
         append(String.format(Locale.US, "%.2f", price))
     }
+}
+
+fun mapCartToOrder(cart: Cart): Order {
+    return Order(
+        id = cart.id.toString(),
+        orderProducts = cart.cartProducts,
+        total = cart.total,
+        discountedTotal = cart.discountedTotal,
+        userId = cart.userId,
+        totalProducts = cart.totalProducts,
+        totalQuantity = cart.totalQuantity
+    )
 }
